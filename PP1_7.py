74ca2338e36c796d4b655bc93770fb54eba57fcf
Alex-Liang-ICD201-01 <alex.liang3@student.tdsb.on.ca>
2024-09-26 01:56:12 +0000

    commit

--- PP1_7.py
+++ PP1_7.py
@@ -1,25 +1,45 @@
 
 
+
 q1():
   #Write code here
+
+  bool1 = True
 
 q2():
   #Write code here
 
+  integer1 = input("Input an integer: ")
+  bool1 = (5 < (int(integer1)))
+  print(bool1)
+
 q3():
   #Write code here
+
+  letter1 = input("Input the letter a: ")
+  bool1 = ('a' == letter1)
+  print(bool1)
 
 q4():
   #Write code here
 
+  word1 = input("Input a word earlier in the dictionary than google: ")
+  bool1 = (word1 < "google")
+  print(bool1)
+
 q5():
   #Write code here
+
+  integer2 = input("Input an integer: ")
+  integer3 = input("Input another integer: ")
+  bool1 = (int(integer2) * int(integer3) > 40)
+  print(f"Your numbers multiplied together are greater than 40: {bool1}")
 
 #Do edit the code below
 #Comment the lines below when running your tests
 
-q1()
-q2()
-q3()
-q4()
-q5()
+# q1()
+# q2()
+# q3()
+# q4()
+# q5()
